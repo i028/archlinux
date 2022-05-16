f3c7d9f64c845718503428367d845e1e9de26df6
coss <25678@i028.cc>
2022-05-16 11:56:59 +0800

    39 2

--- afterefi.sh
+++ afterefi.sh
@@ -35,9 +35,7 @@ pacman -S ttf-dejavu ttf-droid noto-fonts noto-fonts-extra noto-fonts-emoji noto
 pacman -S xorg xorg-xinit --noconfirm
 
 installDWM(){
-	#whiptail --title "Install DWM" --infobox "\n WAITTING PLEASE" 12 35
-
-    pacman -S xorg xorg-xinit dolphin konsole firefox gwenview ntfs-3g ksystemlog ark kcalc kcolorchooser kate flameshot alacritty feh fcitx5-im fcitx5-rime fcitx5-chinese-addons fcitx5-material-color fcitx5-nord rofi picom rxvt-unicode krita archlinux-wallpaper 1> ./errorfile || funerror "pacmanerror" 2
+    pacman -S dolphin konsole firefox gwenview ntfs-3g ksystemlog ark kcalc kcolorchooser kate flameshot alacritty feh fcitx5-im fcitx5-rime fcitx5-chinese-addons fcitx5-material-color fcitx5-nord rofi picom rxvt-unicode krita archlinux-wallpaper --noconfirm 1> ./errorfile || funerror "pacmanerror" 2
 	echo "GTK_IM_MODULE=fcitx
 QT_IM_MODULE=fcitx
 XMODIFIERS=@im=fcitx
