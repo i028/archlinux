ee58a03cfe2fe6f4ec59e9d083ce1d425209cc7f
coss <25678@i028.cc>
2022-05-16 08:12:46 +0800

    add vscodium

--- afterefi.sh
+++ afterefi.sh
@@ -40,7 +40,7 @@ pacman -S alsa-utils pulseaudio pulseaudio-bluetooth cups --noconfirm 1> /dev/nu
 
 installDWM(){
     whiptail --title "Install DWM" --infobox "\n WAITTING PLEASE" 12 35
-    pacman -S xorg xorg-xinit dolphin konsole firefox gwenview ntfs-3g ksystemlog ark kcalc kcolorchooser kate flameshot alacritty feh fcitx5-im fcitx5-rime fcitx5-chinese-addons fcitx5-material-color fcitx5-nord rofi picom rxvt-unicode krita archlinux-wallpaper --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
+    pacman -S xorg xorg-xinit dolphin konsole vscodium firefox gwenview ntfs-3g ksystemlog ark kcalc kcolorchooser kate flameshot alacritty feh fcitx5-im fcitx5-rime fcitx5-chinese-addons fcitx5-material-color fcitx5-nord rofi picom rxvt-unicode krita archlinux-wallpaper --noconfirm 1> /dev/null 2> ./errorfile || funerror "pacmanerror" 2
     cp /etc/X11/xinit/xinitrc ../.xinitrc
     sed -i "/geometry/d" ../.xinitrc && sed -i "s/twm \&/\n\nfeh --bg-fill --randomize \/usr\/share\/backgrounds\/archlinux\/*\n\npicom \&\n\n\nexec dwm/g" ../.xinitrc
     git clone https://gitee.com/cosss/adwm &> /dev/null && cd adwm
@@ -48,18 +48,6 @@ installDWM(){
     cd ../dmenu && make clean install &> /dev/null
     cd ../st && make clean install &> /dev/null
     cd ../../
-#		echo "GTK_IM_MODULE	DEFAULT=fcitx5
-#QT_IM_MODULE	DEFAULT=fcitx5
-#XMODIFIERS	DEFAULT=@im=fcitx5
-#INPUT_METHOD DEFAULT=fcitx5
-#SDL_IM_MODULE	DEFAULT=fcitx5" >> .pam_environment
-
-#	echo "GTK_IM_MODULE=fcitx
-#QT_IM_MODULE=fcitx
-#XMODIFIERS=@im=fcitx
-#INPUT_METHOD=fcitx
-#SDL_IM_MODULE=fcitx
-#GLFW_IM_MODULE=ibus" >> .pam_environment 
 #	mkdir -p .config/alacritty && cp /usr/share/doc/alacritty/example/alacritty.yml .config/alacritty/
 #	sed -i "s/#font:/font:/g" .config/alacritty/alcritty.yml && sed -i "s/#size: 11\.0/size: 13\.0/g" .config/alacritty/alacritty.yml
 	#mkdir -p .config/picom && cp /etc/xdg/picom.conf .config/picom/
